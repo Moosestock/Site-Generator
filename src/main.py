3521e7661658c1ea22f29933f83014f4c46d5dd7
moosestock <83773849+Moosestock@users.noreply.github.com>
2025-06-19 11:01:07 -0400

    yeah

--- src/main.py
+++ src/main.py
@@ -6,11 +6,11 @@ from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
 
 
-dir_path_static = "./static"
-dir_path_public = "./docs"
-dir_path_content = "./content"
-template_path = "./template.html"
-default_basepath = "/"
+dir_path_static = "../static"
+dir_path_public = "../docs"
+dir_path_content = "../content"
+template_path = "../template.html"
+default_basepath = "../"
 
 
 def main():
